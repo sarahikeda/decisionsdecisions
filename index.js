186652304a1e634b76c3aee406588316c0053b16
Sarah Ing <sarahikeda@gmail.com>
2016-11-13 17:50:23 -0600

    Begin dropdown logic for emotion

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
-    var emotion = $('.emotion-dropdown').val();
-    console.log(emotion);
+    $('.energy-dropdown').change(function() {
+      var energyLevel = this.value;
+      var emotion = $('.emotion-dropdown').val();
+      console.log(emotion);
+      console.log(energyLevel);
+      if (!emotion) {
+        console.log('Put Something');
+      } else if (emotion == 'happy') {
+        $('.activity').text('do nothing.')
+      } else {
+        $('.activity').text('jump up and down!')
+      }
+    })
   });
