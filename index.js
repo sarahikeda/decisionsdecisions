c053a1ee1ec89bdfac248e9f7946b6bb702eb861
sarahikeda <sarahikeda@gmail.com>
2017-08-09 08:59:15 -0700

    Update json with more options and display on html

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ var json = {
         "meditate.",
         "do yoga."
       ],
+      "reenergy": [
+        "take a foster dog out for a walk!",
+        "drink some mate"
+      ],
       "lazy": [
         "netflix and chill.",
         "take a nice nap."
@@ -22,6 +26,10 @@ var json = {
       "zen":[
         "visit a monastery.",
         "go for a nature walk."
+      ],
+      "reenergy": [
+        "light a candle and do some journaling",
+        "call a loved one and tell them something you're grateful for"
       ],
       "lazy":[
         "write in a journal.",
@@ -37,6 +45,10 @@ var json = {
         "write about something I'm grateful for.",
         "hike my favorite hike."
       ],
+      "reenergy": [
+        "change the context and get outside.",
+        "pick a recipe to try out in the kitchen."
+      ],
       "lazy":[
         "watch a sad movie and let it all out.",
         "hug a loved one."
@@ -51,13 +63,53 @@ var json = {
         "feel contradictory.",
         "really examine what I'm feeling and why."
       ],
+      "reenergy": [
+        "sign up for kickboxing",
+        "eat a really spicy pepper"
+      ],
       "lazy":[
         "draw an angry picture.",
         "call a friend."
       ]
     },
+    "envious": {
+      "high": [
+        "count to 10 over and over.",
+        "write out why I'm feeling this way."
+      ],
+      "zen":[
+        "feel contradictory.",
+        "make a gong and hit it."
+      ],
+      "reenergy": [
+        "take a self defense class.",
+        "list out my top qualities."
+      ],
+      "lazy":[
+        "make a list of to-dos.",
+        "watch a murder movie."
+      ]
+    },
+    "emotionless": {
+      "high": [
+        "take a break from being so on the go.",
+        "come up with a dance choreography."
+      ],
+      "zen":[
+        "embrace mindfulness.",
+        "find some watercolors and use 'em'."
+      ],
+      "reenergy": [
+        "volunteer at an organization that helps refugees.",
+        "try cooking something with an ingredient I've never used."
+      ],
+      "lazy":[
+        "do a lesson or two on Duolingo.",
+        "go to a community theatre play."
+      ]
+    }
   }
-  };
+};
 
 function getAllActivities(emotion,energyLevel) {
   return json.emotion[emotion][energyLevel]
